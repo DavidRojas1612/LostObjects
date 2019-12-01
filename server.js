7270d1ab76bc6ca8bd799be41a581a311081a1de
David Rojas <david_rojas82142@elpoli.edu.co>
2019-12-01 18:35:12 -0500

    fff

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const { ApolloServer } = require('apollo-server'),
   logger = require('./app/logger'),
   schema = require('./app/graphql');
 
-new ApolloServer({ schema, introspection: true, playground: true, cors: true })
-  .listen({ port: process.env.PORT || 8080 })
-  .then(({ url, subscriptionsUrl }) => {
-    logger.info(`🚀 Server ready at ${url}`);
-    logger.info(`🚀 Subscriptions ready at ${subscriptionsUrl}`);
-  });
+const server = new ApolloServer({ schema, introspection: true, playground: true, cors: true });
+
+server.listen({ port: process.env.PORT || 8080 }).then(({ url, subscriptionsUrl }) => {
+  logger.info(`🚀 Server ready at ${url}`);
+  logger.info(`🚀 Subscriptions ready at ${subscriptionsUrl}`);
+});
